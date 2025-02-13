3de8c52d1e071ebc29a44d26ad273e2eacb31da3
M Tarek <41743928+mutarek@users.noreply.github.com>
2025-02-13 18:04:31 +0600

    rilac pos

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-  //rilac pos  cascka ckask
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
