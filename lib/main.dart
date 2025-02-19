f9d1025f24aa7beaa67774db6e7a8798000358fb
M Tarek <41743928+mutarek@users.noreply.github.com>
2025-02-19 08:56:29 +0600

    home page rilac

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: const MyHomePage(),
     );
